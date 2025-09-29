1f291e6ad1c0057a2c809212f0e9b8e108a0b695
Paola Novick <paola.novick@davinci.edu.ar>
2025-09-29 16:26:12 -0300

    sin texto el h1 de inicio

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -23,7 +23,7 @@ export default function HeroCarousel({ onClick }) {
       {/* Texto centrado */}
       <div className="absolute inset-0 flex items-center justify-center">
         <h1 className="text-white text-4xl md:text-6xl font-bold drop-shadow-lg text-center">
-          Lorem Ipsum Lorem Ipsum
+          
         </h1>
       </div>
     </section>
